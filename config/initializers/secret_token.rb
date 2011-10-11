45259ce68789d57ec044363672c51de7561db810
Peter Shih <ptshih@mini7.local>
2011-10-11 16:59:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phototime::Application.config.secret_token = 'df48e8436988d78dfd3c253ea2f5d38b2053492fd5b7b4b5d0ebc99dfb4ef75402e237fc6fb8bbb550c14a52e278b0677fab52146ef55b5842dc1f637cbb9585'
